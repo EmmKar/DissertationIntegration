952e60150b9901a1f0664efe48db262227b51fba
Emma Karlsmose <emma.karlsmose@hotmail.com>
2022-05-11 00:18:19 +0100

    added program to parse a plan consisting of movement between waypoints

---
+++ src/parse-plan.py
@@ -0,0 +1,114 @@
+# Experimental file to accept waypoints from simulation, 
+# and plan from model, and create a dict linking the pddl name (w1x, w2y, w2x...)
+# and the actual values of the coordinates
+# Note: ROS is needed for this to run. 
+
+# Author: Emma Karlsmose
+# Last Edited: 10 May 2022
+
+import rospkg
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+class ParseNavigation():
+    def __init__(self):
+
+        #Specify names of plan file and file with the waypoints that was copied to pddl
+        self.planName = "/pddl/plans/plan.txt"
+        self.pddlWaypoints = "/src/waypoint-lookup.txt"
+        self.outputFile = "/waypoints/plan_waypoints"
+
+        #Get path to project
+        self.rospack = rospkg.RosPack()
+        self.projectPath = self.rospack.get_path('inspection_plan')
+
+        self.planFile = self.getFile(self.planName, 'r')
+        self.waypointFile = self.getFile(self.pddlWaypoints, 'r')
+
+        simplePlan = self.getPlanList(self.planFile)
+        wpDict = self.getNames(self.waypointFile)
+        plan = self.replaceNames(simplePlan, wpDict)
+        self.createCSV(plan)
+        #self.printPath(plan)
+
+    #Turn data in waypoint lookup file into a dictionary
+    def getNames(self, lookupFile):
+        return dict(x.strip().split('=') for x in lookupFile)
+
+    #Returns a list consisting only of waypoint names per entry
+    #First two coordinates are current waypoint, the latter two are the goal waypoint
+    def getPlanList(self, rawPlan):
+        newPlan = []
+        finalPlan = []
+        actions = ['move', 'turn']
+        
+        # Remove uneccesary characters from plan
+        for step in rawPlan:
+            for x in actions:
+                step = step.replace(x, '')
+            newPlan.append(step.strip('\n()  '))
+        
+        # remove current waypoints from plan
+        for step in newPlan:
+            step = step.split(" ")
+            finalPlan.append(step[2:])
+        return(finalPlan)
+
+    #import textfile and parse into list
+    def getFile(self, fileName, mode):
+        #Open files 
+        file = open(self.projectPath + fileName, mode)
+
+        #Create list from lines
+        lineList = file.readlines()
+
+        return lineList
+
+    def replaceNames(self, plan, wpDict):
+        newPlan = []
+        for step in plan:
+            newPlan.append([float(wpDict.get(wp)) for wp in step])
+        return newPlan
+        
+    #Save parsed navigation plan to CSV 
+    def createCSV(self, plan):
+        df = pd.DataFrame(plan)
+        df.to_csv(self.projectPath + self.outputFile + ".csv", index = False, header = False)
+
+    def printPath(self, plan):
+        df = pd.DataFrame(plan)
+
+        u = np.diff(df[0])
+        v = np.diff(df[1])
+
+        startx = df.loc[0][0]
+        starty = df.loc[0][1]
+
+        plotName = input("What would you like to name your figure?\n")
+        plt.figure()
+        plt.title("Robot Path")
+        plt.scatter(startx, starty, c='green', zorder=3)
+        plt.plot(df[0], df[1], '.', color='orange', zorder=2)
+        plt.quiver(df[0][:-1], df[1][:-1], u, v, scale_units='xy', angles='xy', scale=1, width=0.002, headwidth=9, headlength=10, zorder=1)
+        plt.ylim(ymax=10, ymin=-10)
+        plt.xlim(xmax=10, xmin=-10)
+        plt.xticks(range(-9,10,3))
+        plt.yticks(range(-9,10,3))
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.savefig(self.projectPath + "/src/figures/" + plotName + ".png")
+        plt.show()
+
+
+        
+
+if __name__ == '__main__':
+    test = ParseNavigation()
+#     plan = test.getPlanList(test.planFile)
+#     print(*plan, sep = "\n")
+#     wpDict = test.getNames(test.waypointFile)
+#     plan = test.replaceNames(plan, wpDict)
+#     print(*plan, sep = "\n")
+#     test.createCSV(plan)
+#     test.printPath(plan)
--- src/parse-waypoints-to-pddl.py
+++ src/parse-waypoints-to-pddl.py
@@ -17,17 +17,35 @@ waypoints = pd.read_csv(filePath,
                         names = ['x', 'y', 'z', 'roll', 'pitch', 'yaw', 'w'])
 #print(waypoints)
 
+#save waypoints to file, formatted to easily initialise in PDDL model
+#save waypoints to file, formatted to keep track of which name is 
+#   which waypoint
+pddl_waypoints = open("pddl-waypoints.txt", "w")
+waypoint_lookup = open("waypoint-lookup.txt", "w")
+for i, x in waypoints['x'].iteritems():
+    pddl_waypoints.write("(= (waypoint w{}x) {}) \n".format(i+1, x))
+    waypoint_lookup.write("w{}x={}\n".format(i+1, x))
+for i, y in waypoints['y'].iteritems():
+    pddl_waypoints.write("(= (waypoint w{}y) {}) \n".format(i+1, y))
+    waypoint_lookup.write("w{}y={}\n".format(i+1, y))
+for i, x in waypoints['x'].iteritems():
+    pddl_waypoints.write("(is-waypoint w{}x w{}y) \n".format(i+1, i+1))
+pddl_waypoints.close()
+waypoint_lookup.close()
+
+
+
 def test():
     position = waypoints.drop(['roll', 'pitch', 'yaw', 'w'], axis=1)
     orientation = waypoints.drop(['x', 'y', 'z'], axis=1)
     return position, orientation
 
 mytest = test()
-print(mytest)
+#print(mytest)
 
 ## As we are only interested in a 2D plot, we only need x and y points.
 waypoints = waypoints.drop(['z', 'roll', 'pitch', 'yaw', 'w'], axis=1)
-print(waypoints)
+#print(waypoints)
 
 
 ## Create plot. LATER can save an image of the map and overlay it into the plot so it is clearer where the waypoints are. 
@@ -40,5 +58,5 @@ plt.xticks(range(-9,10,3))
 plt.yticks(range(-9,10,3))
 plt.xlabel("x")
 plt.ylabel("y")
-plt.savefig("waypoint_plot.png")
+plt.savefig("figures/waypoint_plot.png")
 plt.show()
