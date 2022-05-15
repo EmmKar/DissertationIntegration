7ed991152fafd54cde94fa8ef5d32d678476f4bc
Emma Karlsmose <emma.karlsmose@hotmail.com>
2022-05-15 17:04:30 +0100

    added new domain and problem with correct distance expression

--- src/parse-waypoints-to-pddl.py
+++ src/parse-waypoints-to-pddl.py
@@ -9,28 +9,43 @@ import rospkg
 rospack = rospkg.RosPack()
 waypointsFile = input("Name of input waypoint file (excl extension): ")
 # waypointsFile = 'test.csv'
-filePath = rospack.get_path('inspection_plan') + '/waypoints/' + waypointsFile + ".csv"
+inputPath = rospack.get_path('inspection_plan') + '/waypoints/' + waypointsFile + ".csv"
+outputPath = rospack.get_path('inspection_plan') + "/src/outputs/"
 
 ## Orientation is represented as a quaternion, where we have that yaw is rotation around z axis, roll around x and pitch around y, 
 ## and w indicates the amount of rotation which will accur about the axis
-waypoints = pd.read_csv(filePath,
-                        names = ['x', 'y', 'z', 'roll', 'pitch', 'yaw', 'w'])
+waypoints = pd.read_csv(inputPath,
+                        names = ['x', 'y', 'z', 'roll', 'pitch', 'yaw', 'w', 'isInteresting'])
 #print(waypoints)
 
-#save waypoints to file, formatted to easily initialise in PDDL model
-#save waypoints to file, formatted to keep track of which name is 
-#   which waypoint
-outputPath = rospack.get_path('inspection_plan') + "/src/outputs/"
+#save waypoints to a file, formatted to easily initialise in PDDL model
+#save waypoints to a file, formatted to keep track of which name is which waypoint
+
+#the many for-loops are for readability in the pddl code, at cost of speed of this code
 pddl_waypoints = open(outputPath + "pddl-" + waypointsFile + ".txt", "w")
-waypoint_lookup = open(outputPath + waypointsFile + "-lookup.txt", "w")
+waypoint_lookup = open(outputPath + "lookup-" + waypointsFile + ".txt", "w")
+
+#Format x coordinates
 for i, x in waypoints['x'].iteritems():
     pddl_waypoints.write("(= (waypoint w{}x) {}) \n".format(i+1, x))
     waypoint_lookup.write("w{}x={}\n".format(i+1, x))
+
+#Format y coordinates
 for i, y in waypoints['y'].iteritems():
     pddl_waypoints.write("(= (waypoint w{}y) {}) \n".format(i+1, y))
     waypoint_lookup.write("w{}y={}\n".format(i+1, y))
+
+#Format PDDL code to init valid waypoints
 for i, x in waypoints['x'].iteritems():
     pddl_waypoints.write("(is-waypoint w{}x w{}y) \n".format(i+1, i+1))
+
+#Format PDDL code to init interesting objects
+num = 0
+for i, x in waypoints['x'].iteritems():
+    if waypoints.iloc[i]['isInteresting'] == True:
+        num += 1
+        pddl_waypoints.write("(by-interesting w{}x w{}y object{})\n".format(i+1, i+1, num))
+
 pddl_waypoints.close()
 waypoint_lookup.close()
 
@@ -50,7 +65,7 @@ waypoints = waypoints.drop(['z', 'roll', 'pitch', 'yaw', 'w'], axis=1)
 
 create_figure = input("Would you like to plot these waypoints? (y/n) ")
 if create_figure.lower() == 'y':
-    plotName = input("What would you like to name the plot? (excl extension): ")
+    plotName = waypointsFile
     ## Create plot. LATER can save an image of the map and overlay it into the plot so it is clearer where the waypoints are. 
     plt.figure()
     plt.title("Waypoint Plot: " + plotName)
@@ -62,4 +77,8 @@ if create_figure.lower() == 'y':
     plt.xlabel("x")
     plt.ylabel("y")
     plt.savefig(rospack.get_path('inspection_plan') + "/src/figures/" + plotName + ".png")
+    print("Plot saved to /src/figures/"+plotName+".png")
     plt.show()
+
+else:
+    print("No plot created.")
